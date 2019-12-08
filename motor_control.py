e9d49100c11e6a1cfb652b1c20cb1547f99eee3e
Harlen Bains <himalayanelixir@gmail.com>
2019-12-08 13:53:03 -0800

    Fixed thread conflict issues

--- motor_control.py
+++ motor_control.py
@@ -9,19 +9,23 @@ app = Flask(__name__)
 ask = Ask(app, '/')
  
 def moveup():
+  currently_running = 1
   GPIO.output(9,GPIO.HIGH)
   GPIO.output(10,GPIO.HIGH)
   time.sleep(30)
   GPIO.output(9,GPIO.LOW)
   GPIO.output(10,GPIO.LOW)
+  currently_running = 0
   sys.exit()
 
 def movedown():
+  currently_running = 1
   GPIO.output(9,GPIO.HIGH)
   GPIO.output(10,GPIO.LOW)
   time.sleep(30)
   GPIO.output(9,GPIO.LOW)
   GPIO.output(10,GPIO.LOW)
+  currently_running = 0
   sys.exit()
 
 @ask.intent('LedIntent')
@@ -29,26 +33,30 @@ def led(direction):
   #if color.lower() not in pins.keys():
   #  return statement("I don't have {} light".format(color)) 
   print ('Direction:', direction)
-  if direction == 'up':
-    thread = Thread(target=moveup)
-    thread.start()
-    return statement('The couch is now moving {} '.format(direction))
-    
-  elif direction == 'down':
-    thread = Thread(target=movedown)
-    thread.start()
-    return statement('The couch is now moving {} '.format(direction))
-    
+  if currently_running == 0:
+    if direction == 'up':
+      thread = Thread(target=moveup)
+      thread.start()
+      return statement('The couch is now moving {} '.format(direction))
+      
+    elif direction == 'down':
+      thread = Thread(target=movedown)
+      thread.start()
+      return statement('The couch is now moving {} '.format(direction))
+      
+    else:
+      GPIO.output(9,GPIO.LOW)
+      GPIO.output(10,GPIO.LOW)
+      GPIO.output(11,GPIO.LOW)
+      return statement('Invalid Direction')
   else:
-    GPIO.output(9,GPIO.LOW)
-    GPIO.output(10,GPIO.LOW)
-    GPIO.output(11,GPIO.LOW)
-    return statement('Invalid Direction')
+    return statement('The couch is currently moving please try again in a few moments'))
   
  
 if __name__ == '__main__':
   try:
     GPIO.setmode(GPIO.BCM)
+    currently_running = 0
     pins = {9,10,11}
     for pin in pins: 
       GPIO.setup(pin, GPIO.OUT)
