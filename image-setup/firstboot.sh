f39d09579d57070f34b8193b296d10edcd03455a
Harlen Bains <himalayanelixir@gmail.com>
2020-04-09 17:05:00 -0700

    uploaded image setup files

---
+++ image-setup/firstboot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# gives a bit of time for the wifi to connect
+sleep 20
+# enable ssh
+sudo systemctl enable ssh
+# update and upgrade 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+# install programs
+sudo apt-get install expect git zsh python3-pip python3-venv -y
+# change default shell for root and pi users
+sudo chsh -s /bin/zsh pi
+sudo chsh -s /bin/zsh
+# install ohmyzsh for root and pi users
+sudo -u pi sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# tell pi to restart after one minute. This is needed for the ssh changes to work and for the adafruit screen drivers
+sudo shutdown -r 1
